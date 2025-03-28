e2f62a8a3c8237c12fe5156f7c77c68ff9f1ea21
danniel917 <danniel917@hotmail.com>
2025-03-28 13:32:41 -0500

    create static components

---
+++ src/utils/tracks.js
@@ -0,0 +1,34 @@
+const tracks = [
+  {
+    id: "1",
+    name: "Blinding Lights",
+    artist: "The Weeknd",
+    album: "After Hours",
+  },
+  {
+    id: "2",
+    name: "Shape of You",
+    artist: "Ed Sheeran",
+    album: "Divide",
+  },
+  {
+    id: "3",
+    name: "Levitating",
+    artist: "Dua Lipa",
+    album: "Future Nostalgia",
+  },
+  {
+    id: "4",
+    name: "Uptown Funk",
+    artist: "Mark Ronson ft. Bruno Mars",
+    album: "Uptown Special",
+  },
+  {
+    id: "5",
+    name: "Someone Like You",
+    artist: "Adele",
+    album: "21",
+  },
+];
+
+export { tracks };
