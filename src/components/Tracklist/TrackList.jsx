61a1d3fa167bece5f9357ef372000a71c522fc69
danniel917 <danniel917@hotmail.com>
2025-03-28 13:38:38 -0500

    create tracklist and render tracks on tracklist component

--- src/components/Tracklist/TrackList.jsx
+++ src/components/Tracklist/TrackList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const TrackList = () => {
-  return <div>TrackList</div>;
+const TrackList = ({ name, artist, album}) => {
+  return (
+    <>
+      <div>
+        <h2>{name}</h2>
+        <p>{artist}</p>
+        <p>{album}</p>
+      </div>
+    </>
+  );
 };
 
 export { TrackList };
